579689d12566bfdb6d93de5b99c00188eb589e35
anonymze <anodevfr@gmail.com>
2025-02-12 23:36:04 +0100

    ini

--- app/edit.tsx
+++ app/edit.tsx
@@ -1,9 +1,10 @@
-import Animated, { FadeIn, FadeInDown, FadeInUp } from "react-native-reanimated";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
 import { InputTextGradient } from "@/components/text-gradient";
 import { Pressable } from "react-native-gesture-handler";
 import { XIcon, CheckIcon } from "lucide-react-native";
 import LayoutBackground from "@/layout/background";
+import { stylesLayout } from "@/layout/background";
+import { TextInput, View } from "react-native";
 import { router } from "expo-router";
 import { Image } from "expo-image";
 import { useRef } from "react";
@@ -19,20 +20,20 @@ export default function Index() {
 				inputRef.current?.focus();
 			}}
 		>
-			<View id="container" style={styles.container}>
+			<View id="container" style={stylesLayout.container}>
 				<Animated.View entering={FadeInDown.duration(800).delay(200).springify()}>
-					<Image style={styles.image} source="https://picsum.photos/seed/696/3000/2000" />
+					<Image style={stylesLayout.image} source="https://picsum.photos/seed/696/3000/2000" />
 				</Animated.View>
 				<Animated.View entering={FadeInDown.duration(800).delay(150).springify()}>
 					<InputTextGradient text={"Coucou"} style={{ fontSize: 60 }} ref={inputRef} maxLength={8} />
 				</Animated.View>
 			</View>
 			<Animated.View
-				style={[styles.topButtons, styles.topLeftButton]}
+				style={[stylesLayout.topButtons, stylesLayout.topLeftButton]}
 				entering={FadeInDown.duration(800).delay(200).springify()}
 			>
 				<Pressable
-					style={[styles.container, styles.paddingTopButtons]}
+					style={stylesLayout.paddingTopButtons}
 					onPress={() => {
 						router.push("/");
 					}}
@@ -41,66 +42,13 @@ export default function Index() {
 				</Pressable>
 			</Animated.View>
 			<Animated.View
-				style={[styles.topButtons, styles.topRightButton]}
+				style={[stylesLayout.topButtons, stylesLayout.topRightButton]}
 				entering={FadeInDown.duration(800).delay(200).springify()}
 			>
-				<Pressable>
+				<Pressable style={stylesLayout.paddingTopButtons}>
 					<CheckIcon size={28} color="#fff" />
 				</Pressable>
 			</Animated.View>
 		</LayoutBackground>
 	);
 }
-
-const styles = StyleSheet.create({
-	topButtons: {
-		position: "absolute",
-		top: 35,
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-		width: 45,
-		aspectRatio: 1,
-		borderRadius: 12,
-		backgroundColor: "#1487ff",
-		boxShadow: "inset 0 0 8px 0 #fff",
-	},
-	topLeftButton: {
-		left: 30,
-	},
-	topRightButton: {
-		right: 30,
-	},
-	topMiddleButton: {
-		left: "50%",
-		transform: [{ translateX: "-50%" }],
-	},
-	image: {
-		width: 130,
-		aspectRatio: 1,
-		borderRadius: 99,
-		marginBottom: 3,
-	},
-	bottomButton: {
-		position: "absolute",
-		bottom: 50,
-		width: "100%",
-		maxWidth: 300,
-		borderRadius: 99,
-		padding: 16,
-		backgroundColor: "red",
-		borderTopColor: "#fff",
-		borderTopWidth: 2,
-		color: "#fff",
-		alignItems: "center",
-	},
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	paddingTopButtons: {
-		paddingBlock: 30,
-		paddingHorizontal: 20,
-	},
-});
--- layout/background.tsx
+++ layout/background.tsx
@@ -31,3 +31,55 @@ const styles = StyleSheet.create({
 		flex: 1,
 	},
 });
+
+export const stylesLayout = StyleSheet.create({
+	topButtons: {
+		position: "absolute",
+		top: 35,
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		width: 45,
+		aspectRatio: 1,
+		borderRadius: 12,
+		backgroundColor: "#1487ff",
+		boxShadow: "inset 0 0 8px 0 #fff",
+	},
+	topLeftButton: {
+		left: 30,
+	},
+	topRightButton: {
+		right: 30,
+	},
+	topMiddleButton: {
+		left: "50%",
+		transform: [{ translateX: "-50%" }],
+	},
+	image: {
+		width: 130,
+		aspectRatio: 1,
+		borderRadius: 99,
+		marginBottom: 3,
+	},
+	bottomButton: {
+		position: "absolute",
+		bottom: 50,
+		width: "100%",
+		maxWidth: 300,
+		borderRadius: 99,
+		padding: 16,
+		backgroundColor: "red",
+		borderTopColor: "#fff",
+		borderTopWidth: 2,
+		color: "#fff",
+		alignItems: "center",
+	},
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	paddingTopButtons: {
+		padding: 25,
+	},
+});
