1aa6df5939d046de383753b8e1e52efa4427a541
anonymze <anodevfr@gmail.com>
2025-02-13 12:35:17 +0100

    ini

--- app/edit.tsx
+++ app/edit.tsx
@@ -97,7 +97,7 @@ const styles = StyleSheet.create({
 	shadowImage: {
 		shadowColor: "#000",
 		shadowOffset: { width: -1, height: 1 },
-		shadowOpacity: 0.3,
+		shadowOpacity: 0.2,
 		shadowRadius: 2,
 	},
 });
