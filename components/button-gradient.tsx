9aab752cab0c0b4457690c29dd1021568f909b92
anonymze <anodevfr@gmail.com>
2025-02-13 23:47:02 +0100

    ini

--- components/button-gradient.tsx
+++ components/button-gradient.tsx
@@ -1,25 +1,44 @@
+import Svg, { Defs, RadialGradient, Stop, Rect } from "react-native-svg";
 import { Pressable, PressableProps } from "react-native-gesture-handler";
-import { LinearGradient } from "react-native-linear-gradient";
 import { StyleSheet, Text } from "react-native";
 
 
-export function ButtonGradient(props: PressableProps & { children: React.ReactNode }) {
+export function ButtonGradient({ text, color, ...props }: PressableProps & { text: string, color: string }) {
 	return (
-		<Pressable {...props}>
-			<LinearGradient colors={["#000", "#fff"]} style={styles.button}>
-				<Text style={styles.text}>{props.children}</Text>
-			</LinearGradient>
+		<Pressable {...props} style={styles.container}>
+			<Svg style={{ position: "absolute", width: "100%", height: "100%" }}>
+				<Defs>
+					<RadialGradient
+						id="grad"
+						cx="50%"
+						cy="0%"
+						rx="100%"
+						ry="100%"
+						fx="50%"
+						fy="0%"
+						// gradientUnits="userSpaceOnUse"
+					>
+						<Stop offset="20%" stopColor="#fff" stopOpacity="0.8" />
+						<Stop offset="100%" stopColor={color} stopOpacity="0.9" />
+					</RadialGradient>
+				</Defs>
+				<Rect x="0" y="0" width="100%" height="100%" fill="url(#grad)" rx="27.5" ry="27.5" />
+			</Svg>
+			<Text style={styles.text}>{text}</Text>
 		</Pressable>
 	);
 }
 
 const styles = StyleSheet.create({
-	button: {
-		borderRadius: 99,
-		padding: 16,
-		backgroundColor: "red",
+	container: {
+		width: 325,
+		height: 55,
+		alignItems: "center",
+		justifyContent: "center",
 	},
 	text: {
 		color: "#fff",
+		fontSize: 18,
+		fontWeight: "bold",
 	},
 });
