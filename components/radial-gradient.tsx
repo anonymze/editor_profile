0f767a9791776b67eba30154b478887716b9000b
anonymze <anodevfr@gmail.com>
2025-02-15 00:31:36 +0100

    ini

--- components/radial-gradient.tsx
+++ components/radial-gradient.tsx
@@ -101,9 +101,9 @@ export function AnimatedCircleRadialGradient({
 
 		opacity.value = withRepeat(
 			withDelay(
-				3250,
+				3200,
 				withTiming(0, {
-					duration: 250,
+					duration: 300,
 					easing: Easing.linear,
 				})
 			),
