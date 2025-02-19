60b7ee67d99cc82aa71cb7af12b5065c4589bb93
anonymze <anodevfr@gmail.com>
2025-02-19 04:44:24 +0100

    ini

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { DEFAULT_COLOR, DEFAULT_KEY_COLOR, themeColors } from "@/utils/theme-sto
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useMMKVString } from "react-native-mmkv";
-import { Platform, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router";
 
