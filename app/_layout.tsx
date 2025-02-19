662a246aca0776e7bfc72d463b3d1afbeea33899
anonymze <anodevfr@gmail.com>
2025-02-19 01:22:04 +0100

    ini

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,28 @@
 import "react-native-reanimated";
 
+import { DEFAULT_COLOR, DEFAULT_KEY_COLOR, themeColors } from "@/utils/theme-storage";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { getStorageColor, themeColors } from "@/utils/theme-storage";
+import { useMMKVString } from "react-native-mmkv";
 import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router";
 
 
 export default function RootLayout() {
-	const themeColor = getStorageColor();
+	const [themeColor] = useMMKVString(DEFAULT_KEY_COLOR);
+
 	return (
 		<GestureHandlerRootView>
 			<StatusBar style="light" />
 			<SafeAreaProvider>
 				<SafeAreaView
 					edges={["right", "left", "top"]}
-					style={{ flex: 1, backgroundColor: themeColors[themeColor].primaryLight }}
+					style={{ flex: 1, backgroundColor: themeColors[themeColor ?? DEFAULT_COLOR].primaryLight }}
 				>
 					<Stack
 						screenOptions={{
 							headerShown: false,
 							animation: "none",
-							headerStyle: {
-								backgroundColor: "#007AFF", // This will show through the status bar
-							},
-							contentStyle: {
-								backgroundColor: "#A020F0", // Same purple color
-							},
-							headerTintColor: "green",
 						}}
 					>
 						<Stack.Screen options={{ animation: "fade" }} name="recipe" />
