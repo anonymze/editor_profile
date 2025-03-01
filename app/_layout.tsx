76a98ed79afd4ee609f43f98273c45db4ad6ce67
anonymze <anodevfr@gmail.com>
2025-03-01 02:41:32 +0100

    add: signature library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,33 @@ import "react-native-reanimated";
 import { DEFAULT_COLOR, DEFAULT_KEY_COLOR, themeColors } from "@/theme/theme-storage";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from "expo-splash-screen";
 import { useMMKVString } from "react-native-mmkv";
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import React from "react";
 
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+SplashScreen.setOptions({
+	fade: true,
+	duration: 200,
+});
 
 export default function RootLayout() {
 	const [themeColor] = useMMKVString(DEFAULT_KEY_COLOR);
 	const themeColorFinal = (themeColor as keyof typeof themeColors) ?? DEFAULT_COLOR;
+	const [loaded] = useFonts({
+		// AtkinsonRegular: require("@/assets/fonts/atkinson/Atkinson-Hyperlegible-Regular-102a.woff2"),
+		// AtkinsonBold: require("@/assets/fonts/atkinson/Atkinson-Hyperlegible-Bold-102a.woff2"),
+		// AtkinsonItalic: require("@/assets/fonts/atkinson/Atkinson-Hyperlegible-Italic-102a.woff2"),
+	});
+
+	React.useEffect(() => {
+		if (loaded) SplashScreen.hideAsync();
+	}, [loaded]);
 
 	return (
 		<GestureHandlerRootView>
