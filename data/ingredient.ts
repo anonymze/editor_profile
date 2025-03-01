74f361f0846f80a1795d827912bb88ad8161f97e
anonymze <anodevfr@gmail.com>
2025-03-01 13:08:30 +0100

    add: signature library

---
+++ data/ingredient.ts
@@ -0,0 +1,115 @@
+export default [
+	{
+		id: "ingredient_bicarbonate",
+		co2: 0.12,
+		image: require("@/assets/images/ingredients/bicarbonate.svg"),
+		label: {
+			FR: "Bicarbonate",
+		},
+	},
+	{
+		id: "ingredient_cacao",
+		co2: 0.18,
+		image: require("@/assets/images/ingredients/cacao.svg"),
+		label: {
+			FR: "Cacao",
+		},
+	},
+	{
+		id: "ingredient_cannelle",
+		co2: 0.14,
+		image: require("@/assets/images/ingredients/cannelle.svg"),
+		label: {
+			FR: "Cannelle",
+		},
+	},
+	{
+		id: "ingredient_chapelure",
+		co2: 0.16,
+		image: require("@/assets/images/ingredients/chapelure.svg"),
+		label: {
+			FR: "Chapelure",
+		},
+	},
+	{
+		id: "ingredient_chocolat",
+		co2: 0.19,
+		image: require("@/assets/images/ingredients/chocolat.svg"),
+		label: {
+			FR: "Chocolat",
+		},
+	},
+	{
+		id: "ingredient_farine",
+		co2: 0.15,
+		image: require("@/assets/images/ingredients/farine.svg"),
+		label: {
+			FR: "Farine",
+		},
+	},
+	{
+		id: "ingredient_fecule",
+		co2: 0.15,
+		image: require("@/assets/images/ingredients/fecule.svg"),
+		label: {
+			FR: "Fécule",
+		},
+	},
+	{
+		id: "ingredient_gelatine",
+		co2: 0.17,
+		image: require("@/assets/images/ingredients/gelatine.svg"),
+		label: {
+			FR: "Gélatine",
+		},
+	},
+	{
+		id: "ingredient_levure",
+		co2: 0.14,
+		image: require("@/assets/images/ingredients/levure.svg"),
+		label: {
+			FR: "Levure",
+		},
+	},
+	{
+		id: "ingredient_miel",
+		co2: 0.16,
+		image: require("@/assets/images/ingredients/miel.svg"),
+		label: {
+			FR: "Miel",
+		},
+	},
+	{
+		id: "ingredient_moutarde",
+		co2: 0.15,
+		image: require("@/assets/images/ingredients/moutarde.svg"),
+		label: {
+			FR: "Moutarde",
+		},
+	},
+	{
+		id: "ingredient_oeuf",
+		co2: 0.21,
+		image: require("@/assets/images/ingredients/oeuf.svg"),
+		label: {
+			FR: "Oeuf",
+		},
+	},
+	{
+		id: "ingredient_vanille",
+		co2: 0.13,
+		image: require("@/assets/images/ingredients/vanille.svg"),
+		label: {
+			FR: "Vanille",
+		},
+	},
+	
+	{
+		id: "ingredient_vinaigre",
+		co2: 0.13,
+		image: require("@/assets/images/ingredients/vinaigre.svg"),
+		label: {
+			FR: "Vinaigre",
+		},
+	},
+]; 
