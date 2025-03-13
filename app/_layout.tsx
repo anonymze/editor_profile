5579687d417c88f63b82a7a7791691496dce7568
anonymze <anodevfr@gmail.com>
2025-03-13 20:16:31 +0100

    PURCHASES

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,7 +27,7 @@ const apiKey = Platform.select({
 });
 
 Purchases.configure({ apiKey: apiKey || "" });
-Purchases.setLogLevel(LOG_LEVEL.ERROR);
+Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
 /** end initialize RevenueCat */
 
 // ignore warning logs
