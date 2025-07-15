5c2c59178f642f1b6ba8fd1b771ca6ece443586d
anonymze <anodevfr@gmail.com>
2025-07-15 17:07:36 +0200

    fix: excels

--- recipe-api-vercel/api/recipe.ts
+++ recipe-api-vercel/api/recipe.ts
@@ -153,7 +153,7 @@ const generateRecipeWithOpenRouter = async (
 		- Réponds en français
 		- Vouvoie l'utilisateur
 		- Tu ne dois pas proposer une recette qui nécessite des ingrédients que tu n'as pas reçu, à l'exception
-		de certains condiments qui sont très facilement trouvables dans une cuisine française : huile, beurre, sel, poivre... fais bien attention quand tu proposes des ingredients supplémentaires que ce soit bien des condiments
+		de certains condiments qui sont très facilement trouvables dans une cuisine française : huile, beurre, sel, poivre... fais attention quand tu proposes des ingredients supplémentaires que ce soit bien des condiments
 		- Les ingrédients doivent être présentés dans l'ordre alphabétique, avec les ingrédients optionnels en dernier
 		- Tu dois expliquer les termes techniques peu commun que tu emplois, imagine que tu parles à une personne de 20 ans
 		- Le titre de la recette doit être original et non redondant
